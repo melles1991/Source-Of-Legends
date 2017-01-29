47fb0f340aecb9e0fb9eb1d009a8145412d45ae1
krot-melles <melles.android@gmail.com>
2017-01-29 12:42:53 +0200

    update

--- Assets/script/skills/passive/CombatMedecine.cs
+++ Assets/script/skills/passive/CombatMedecine.cs
@@ -6,33 +6,34 @@ using System.Collections;
 namespace statsCM {
 public class CombatMedecine 
 {
-      const int _LEVELCM = 1;
-			public int LevelCm = _LEVELCM;
+      const int _LEVELCM = 1; // начальный уровень скила
+      public int LevelCm = _LEVELCM;
       
-      const float _REGENHP = 0.6; 
-			const float _TIMEREGEN = 10; 
-			public float regenHp = _REGENHP; 
-		  public float timeRegen = _TIMEREGEN;
+      const float _REGENHP = 0.6; // начальное количество востановления HP
+      const float _TIMEREGEN = 10; // время востановления HP
+      public float regenHp = _REGENHP; 
+      public float timeRegen = _TIMEREGEN;
     public Stats()
 		{
 			this.newregenhp ();
 
 		}
-		public Stats() //конструктор класса с о всеми значениями
+    public Stats() //конструктор класса с о всеми значениями
 		{
 			this.LevelCm = LevelCm; //начальный уровень
-			this.regenHp = regenHp;
-			this.timeRegen = timeRegen; 
+			this.regenHp = regenHp; // востановление HP
+			this.timeRegen = timeRegen; //время
 			
-			this.newregenhp (); //считаем кол-во жизни
+			this.newregenhp (); //считаем кол-во HP для востановления
 		}
 
-		public void lvlUPcm() //функция вызываемая при повышении уровня
+    public void lvlUPcm() //функция вызываемая при повышении уровня
 		{
 			this.LevelCm += 1; //уровень устанавливаем +1
 		}
-		public void newregenhp()
+    public void newregenhp()
 		{
-			this.regenHp =this.regenHp  + 0.6; 
+			this.regenHp =this.regenHp  + 0.6; // перещет по уровню
 		}
     }
+}
