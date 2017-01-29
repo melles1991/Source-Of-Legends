450ca95901831ab9c23f6595e3d5190e12bd17c9
Sterben-ПК\Sterben <melles.android@gmail.com>
2017-01-29 21:05:16 +0200

    fix

--- Assets/script/skills/passive/CombatMedecine.cs
+++ Assets/script/skills/passive/CombatMedecine.cs
@@ -9,16 +9,16 @@ public class CombatMedecine
       const int _LEVELCM = 1; // начальный уровень скила
       public int LevelCm = _LEVELCM;
       
-      const float _REGENHP = 0.6; // начальное количество востановления HP
+      const float _REGENHP = 0.6f; // начальное количество востановления HP
       const float _TIMEREGEN = 10; // время востановления HP
       public float regenHp = _REGENHP; 
       public float timeRegen = _TIMEREGEN;
-    public Stats()
+		public CombatMedecine ()
 		{
 			this.newregenhp ();
 
 		}
-    public Stats() //конструктор класса с о всеми значениями
+		public CombatMedecine (int LevelCm) //конструктор класса с о всеми значениями
 		{
 			this.LevelCm = LevelCm; //начальный уровень
 			this.regenHp = regenHp; // востановление HP
@@ -33,7 +33,7 @@ public class CombatMedecine
 		}
     public void newregenhp()
 		{
-			this.regenHp =this.regenHp  + 0.6; // перещет по уровню
+			this.regenHp =this.regenHp  + 6/10; // перещет по уровню
 		}
     }
 }
