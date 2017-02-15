5332fd3ed0a1096908bd9317bb7b23fa5c8a2b0a
Sterben-ПК\Sterben <melles.android@gmail.com>
2017-02-15 09:11:37 +0200

    no message

---
+++ Assets/Cleaver Warrior/Test World/Scripts/HideCursor.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+var showCursor : boolean;
+var lockCursor : boolean;
+
+var toggleKey : KeyCode;
+
+function Start () {
+
+}
+
+function Update () {
+	Screen.showCursor = showCursor;
+	Screen.lockCursor = lockCursor;
+	if(Input.GetKeyDown(toggleKey)){
+		showCursor = !showCursor;
+		lockCursor = !lockCursor;
+	}
+}
